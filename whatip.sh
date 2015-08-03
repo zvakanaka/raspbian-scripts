847a8bc60f0000ab971df13a58709f5a4b775c14
Adam Quinton <quintonish@gmail.com>
2015-08-03 12:15:46 -0600

    Now works on Raspbian
    After testing on Raspbian (previously it was only tested on Ubuntu), there were multiple bugs having to do with the echo command

--- whatip.sh
+++ whatip.sh
@@ -2,31 +2,27 @@
 # whatip.sh
 # By Adam Quinton
 # Email the IP address of the current machine
+# Ssmtp must be previously configured
 
 CONFIG_DIR=$HOME/.whatip
 #contains the TO and FROM emails
 CONFIG_FILE=$CONFIG_DIR/config
 
-# One time setup
-if [ ! -f $CONFIG_FILE ]; then
+# Source variables in config file, or One time setup
+if [ -f $CONFIG_FILE ]; then
+    chmod +x $CONFIG_FILE
+    . $CONFIG_FILE
+else
     if [ ! -d $CONFIG_DIR ]; then
         mkdir $CONFIG_DIR
     fi
-    echo  \#!/bin/bash > $CONFIG_FILE
+    echo -e \#!/bin/bash > $CONFIG_FILE
     read -p "To Email (e.g. linus@gmail.com): " toEmail
     echo export toEmail=$toEmail >> $CONFIG_FILE
     read -p "From Email (e.g. woz@gmail.com): " fromEmail
     echo export fromEmail=$fromEmail >> $CONFIG_FILE
 fi
 
-# test if config file exists and source it
-if [ -f $CONFIG_FILE ]; then
-    chmod +x $CONFIG_FILE
-    . $CONFIG_FILE
-else
-    echo 'ERROR: Missing configuration'
-    exit 1
-fi
 # grab ip address
 IPADDRESSES=$(ifconfig | grep 'inet addr')
 IPTEMPPREFIX=${IPADDRESSES##*inet addr:}
@@ -34,5 +30,5 @@ CURRIP=${IPTEMPPREFIX%%Bcast:*}
 
 echo 'Sending IP: '$CURRIP' FROM: '$fromEmail' TO: '$toEmail''
 
-echo -e 'To: '$toEmail'\nFrom: '$fromEmail'\nSubject: Ip Address of '$USER'\n\nYour ip is '$CURRIP'\nThis message was sent by '$USER' from '$HOSTNAME'\nMessage sent '$(date)'.' > tmpIpEmail.txt
+echo -e 'To: '$toEmail'\nFrom: '$fromEmail'\nSubject: Ip Address of '$HOSTNAME'\n\nYour ip is '$CURRIP'\nThis message was sent by '$USER' from '$HOSTNAME'\nMessage sent '$(date)'.' > tmpIpEmail.txt
 ssmtp $toEmail < tmpIpEmail.txt
