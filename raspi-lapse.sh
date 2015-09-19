5ef5d380c0737a3578f58092158234cc0c654c8f
Adam Quinton <quintonish@gmail.com>
2015-09-19 16:45:43 -0600

    Update raspi-lapse.sh
    Now create video at end and more customizable

--- raspi-lapse.sh
+++ raspi-lapse.sh
@@ -7,19 +7,43 @@ MIN_DISPLAY="-p 1600,100,100,80 -op 200"
 NO_DISPLAY="-n"
 ADJUST="-sh 40 -awb auto -mm average -v"
 
+DURATION="$2"
+FREQ="$1"
+if [ ! "$DURATION" ]; then
+   # Miliseconds to take pictures for: 1000 is 1 Second
+   DURATION=21600000
+   if [ ! "$FREQ" ]; then
+      # Frequency in miliseconds: 1000 is 1 Second                                    
+      FREQ=5000
+   fi
+fi
+
+# Set up Camera Module if not already
 grep "start_x=1" /boot/config.txt > /dev/null 2>&1 || (sed -i "s/start_x=0/start_x=1/g" /boot/config.txt && echo Camera setup now installed, you must reboot && exit 1)
 
 TIMELAPSE_DIR=$HOME/timelapse
 # If no TIMELAPSE_DIR, create one
 [ -d ${TIMELAPSE_DIR} ] || mkdir -p ${TIMELAPSE_DIR}
 
-FREQ="$1"
-if [ ! "$FREQ" ]; then
-    # Frequency in miliseconds: 1000 is 1 Second                                                          
-    FREQ=5000
-fi
-
 # Date Format: Sep-19-2015-12PM                                                      
 DATE=$(date +%b-%d-%Y-%I%p)
 
-raspistill -o ${TIMELAPSE_DIR}/${DATE}-pic%04d.jpg -t 21600000 -tl $FREQ $ADJUST $NO_DISPLAY
+raspistill -o ${TIMELAPSE_DIR}/${DATE}-pic%04d.jpg -t ${DURATION} -tl $FREQ ${ADJUST} ${NO_DISPLAY}
+
+# Checks if first parameter is installed, installs if not
+should_install () {
+    dpkg -s $1 > /dev/null
+    if [ $? -eq 1 ]
+    then
+	echo -e $1 not installed, installing...
+	sudo apt-get install $1
+    else
+	echo $1 already installed
+    fi
+}
+
+cd ${TIMELAPSE_DIR}/
+ls *.jpg > list.txt
+should_install mencoder
+# From fotosyn.com as well
+mencoder -nosound -ovc lavc -lavcopts vcodec=mpeg4:aspect=16/9:vbitrate=8000000 -vf scale=1920:1080 -o timelapse.avi -mf type=jpeg:fps=24 mf://@list.txt
