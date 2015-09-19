afd58127bbcfb83b8c2b55e0e1e3a69dd14bdaf0
Adam Quinton <quintonish@gmail.com>
2015-09-19 12:28:00 -0600

    Update raspi-lapse.sh

--- raspi-lapse.sh
+++ raspi-lapse.sh
@@ -6,11 +6,9 @@ TIMELAPSE_DIR=$HOME/timelapse
 
 FREQ="$1"
 if [ ! "$FREQ" ]; then
-    # Frequency in miliseconds                                                  
-    # 1000 is 1 Second                                                          
+    # Frequency in miliseconds: 1000 is 1 Second                                                          
     FREQ=5000
 fi
 
-# Date Format:                                                                  
-# 12:11:38PM-Sep-19-2015                                                        
+# Date Format: 12:11:38PM-Sep-19-2015                                                        
 raspistill -o ${TIMELAPSE_DIR}/$(date +%I:%M:%S%p-%b-%d-%Y).jpg -tl $FREQ
