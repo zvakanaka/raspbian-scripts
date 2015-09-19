7b4b59febcf6885e10ff75d93e4334f34183e139
Adam Quinton <quintonish@gmail.com>
2015-09-19 14:30:24 -0600

    Update raspi-lapse.sh
    Fixed redirected output and missing time length

--- raspi-lapse.sh
+++ raspi-lapse.sh
@@ -2,7 +2,7 @@
 # raspi-lapse                                                                     
 # Author: Adam Quinton                                                          
 
-grep "start_x=1" /boot/config.txt || (sed -i "s/start_x=0/start_x=1/g" /boot/config.txt && echo Camera setup now installed, you must reboot && exit 1)
+grep "start_x=1" /boot/config.txt > /dev/null 2>&1 || (sed -i "s/start_x=0/start_x=1/g" /boot/config.txt && echo Camera setup now installed, you must reboot && exit 1)
 
 TIMELAPSE_DIR=$HOME/timelapse
 # If no TIMELAPSE_DIR, create one
@@ -15,4 +15,4 @@ if [ ! "$FREQ" ]; then
 fi
 
 # Date Format: 12:11:38PM-Sep-19-2015                                                        
-raspistill -o ${TIMELAPSE_DIR}/$(date +%I:%M:%S%p-%b-%d-%Y).jpg -tl $FREQ
+raspistill -o ${TIMELAPSE_DIR}/$(date +%I:%M:%S%p-%b-%d-%Y).jpg -t 21600000 -tl $FREQ -p 1600,100,100,80 -op 200
