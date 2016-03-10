012a9fca37ce76871cb5e09acef86f2a8503b513
Adam Quinton <quintonish@gmail.com>
2016-03-10 10:21:58 -0700

    Fix order of execution and ensure packages downloaded

--- gphoto2-pi.sh
+++ gphoto2-pi.sh
@@ -3,29 +3,31 @@
 # Author: Adam Quinton
 # 1-19-2016
 # Download and install gphoto
-
+GPHOTO2=gphoto2-2.5.3.tar.bz2
+LIBGPHOTO2=libgphoto2-2.5.3.1.tar.bz2
 mkdir gphoto2
 cd gphoto2
-if [ ! -f gphoto2-2.5.3.tar.bz2 ] ; then
-   wget http://downloads.sourceforge.net/project/gphoto/gphoto/2.5.3/gphoto2-2.5.3.tar.bz2
-fi
-if [ ! -f libgphoto2-2.5.3.1.tar.bz2 ] ; then
+if [ ! -f $LIBGPHOTO2 ] ; then
    wget http://downloads.sourceforge.net/project/gphoto/libgphoto/2.5.3.1/libgphoto2-2.5.3.1.tar.bz2
 fi
 
-tar xvjf gphoto2-2.5.3.tar.bz2
-tar xvjf libgphoto2-2.5.3.1.tar.bz2
-
-cd libgphoto2-2.5.3.1/
-sudo apt-get install libltdl-dev libusb-dev libusb-1.0-0-dev -y
-./configure --prefix=/usr --disable-nls
-sudo make install
-
-cd ..
-
-cd gphoto2-2.5.3/
-sudo apt-get install autotools-dev automake libpopt-dev -y
-./configure --prefix=/usr --disable-nls
-sudo make install
+if [ -f $LIBGPHOTO2 ] ; then
+   if [ ! -f $GPHOTO2 ] ; then
+      wget http://downloads.sourceforge.net/project/gphoto/gphoto/2.5.3/gphoto2-2.5.3.tar.bz2
+   fi
+   tar xvjf $LIBGPHOTO2
+   cd libgphoto2*/
+   sudo apt-get install libltdl-dev libusb-dev libusb-1.0-0-dev -y
+   ./configure --prefix=/usr --disable-nls
+   sudo make install
+   cd ..
+   if [ -f $GPHOTO2 ] ; then
+      tar xvjf $GPHOTO2
+      cd gphoto2*/
+      sudo apt-get install autotools-dev automake libpopt-dev -y
+      ./configure --prefix=/usr --disable-nls
+      sudo make install
+   fi
+fi
 
 gphoto2 --version
